cec97dd807abf52862c52e937ca422c7ee4208d9
smurf <smurf@kakao.com>
2023-10-20 01:47:12 +0900

    added product hard corded dummy

---
+++ src/constants/Products.js
@@ -0,0 +1,37 @@
+export const Products = [
+	{
+		id: 0,
+		name: '빨간 꽃다발',
+		price: 10000,
+		thumbnail: '',
+		category: ['bouquet', 'premium', 'monthly flower'],
+	},
+	{
+		id: 1,
+		name: '노란 꽃다발',
+		price: 15000,
+		thumbnail: '',
+		category: ['bouquet', 'monthly flower'],
+	},
+	{
+		id: 2,
+		name: '명품 꽃바구니',
+		price: 15000,
+		thumbnail: '',
+		category: ['basket', 'monthly flower'],
+	},
+	{
+		id: 3,
+		name: '노란 꽃다발',
+		price: 15000,
+		thumbnail: '',
+		category: ['box', 'monthly flower'],
+	},
+	{
+		id: 4,
+		name: '꽃과자',
+		price: 15000,
+		thumbnail: '',
+		category: ['dessert', 'premium'],
+	},
+];
