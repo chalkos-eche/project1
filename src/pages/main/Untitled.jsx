8146162949a38d8ea34d3ba26a56c3f2331cebe8
chalkos-eche <smurf@kakao.com>
2023-11-01 17:39:58 +0900

    wip

--- src/pages/main/Untitled.jsx
+++ src/pages/main/Untitled.jsx
@@ -4,6 +4,19 @@ import { ItemList } from '../../store/practice';
 
 const Untitled = () => {
 	const { defaultCart, increaseCart, decreaseCart } = useCartStore();
+	// zustand의 불필요한 렌더링 최적화
+	// BEFORE :
+	// const { defaultCart, increaseCart, decreaseCart } = useCartStore();
+	// 모든 store를 가져오기 때문에, 하나의 상태변경으로 모든 컴포넌트가 리렌더링됨
+	// after :
+	// const increaseCart = useCartStore((state)=>state.increaseCart);
+	// const decreaseCart = useCartStore((state)=>state.decreaseCart);
+	// useStore()가 아닌, 컴포넌트에 필요한 액션들만 호출하기.
+	// AFTER :
+	// shallow 를 가져와서 두번쨰 인자에 shallow 등록
+	//
+	// shallow 함수는 객체의 내부 속성 값도 비교함
+	// 메모리 주소가 변경되지 않고 객체 내부값의 변동이 있을 시 리렌더링 변동
 
 	const handleIncrease = value => {
 		increaseCart({
